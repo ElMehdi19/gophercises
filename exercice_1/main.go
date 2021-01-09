ccb4d999e392d10188f89863eff3e8c3b04cfe4f
El Mehdi Rami <elmehdirami5@gmail.com>
2021-01-09 02:40:36 +0100

    exercice_1 part2 done

--- exercice_1/main.go
+++ exercice_1/main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
 )
 
 func exit(msg string) {
@@ -18,43 +19,61 @@ type problem struct {
 }
 
 func parseLines(lines [][]string) []problem {
-	problems := make([]problem, 3)
+	problems := make([]problem, len(lines))
 	for i, line := range lines {
 		problems[i] = problem{q: line[0], a: line[1]}
-		if i == 2 {
-			break
-		}
 	}
 	return problems
 }
 
 func main() {
-	fname := flag.String("csv", "problems.csv", "CSV file in `question,answer` format")
+	// program entrypoint goes here
+	fname := flag.String("csv", "problems.csv", "CSV File in `question,answer` format.")
 	flag.Parse()
 
+	// open file and store file handler in a variable
 	file, err := os.Open(*fname)
 	if err != nil {
-		exit(fmt.Sprintf("Couldn't open file %s", *fname))
+		exit(fmt.Sprintf("Coudln't open file %s", *fname))
 	}
 	defer file.Close()
 
+	// parse csv file
 	r := csv.NewReader(file)
 	lines, err := r.ReadAll()
 	if err != nil {
-		exit(fmt.Sprintf("Couldn't parse file\nError: %s", err))
+		exit(fmt.Sprintf("Couldn't parse file! Error: %s", err))
 	}
 
 	problems := parseLines(lines)
 	counter := 0
+	total := 4
 
+	timer := time.NewTimer(5 * time.Second)
+	fmt.Println("TIMER STARTED, 2 SECONDS TO GO")
 	for i, problem := range problems {
-		fmt.Printf("Problem #%d: %s= ", i+1, problem.q)
-		var answer string
-		fmt.Scanf("%s\n", &answer)
-		if answer == problem.a {
-			counter++
+		if total == i {
+			break
+		}
+
+		answerChan := make(chan string)
+		go func() {
+			fmt.Printf("Problem #%d: %s=", i+1, problem.q)
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerChan <- answer
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Printf("\nYou've scored %d out of %d\n", counter, total)
+			return
+
+		case answer := <-answerChan:
+			if answer == problem.a {
+				counter++
+			}
 		}
 	}
-
-	fmt.Printf("You've scored %d out of %d", counter, len(problems))
+	fmt.Printf("You've scored %d out of %d\n", counter, total)
 }
