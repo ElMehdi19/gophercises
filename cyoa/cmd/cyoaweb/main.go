dfa2e2aa03f34ae8487fef65e231502d02e5a6b6
El Mehdi Rami <elmehdirami5@gmail.com>
2021-01-16 21:38:02 +0100

    cyoa http.Handler and paths parsing done

--- cyoa/cmd/cyoaweb/main.go
+++ cyoa/cmd/cyoaweb/main.go
@@ -1,32 +1,40 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "os"
-
-    "github.com/ElMehdi19/gophercises/cyoa"
+	"flag"
+	"fmt"
+	"os"
+	"log"
+	"net/http"
+	
+	"github.com/ElMehdi19/gophercises/cyoa"
 )
 
-func exit(err error){
-    fmt.Errorf("%s", err)
-    os.Exit(1)
+
+
+func exit(err error) {
+	fmt.Println(fmt.Errorf("%s", err))
+	os.Exit(1)
 }
 
-func main(){
-    fileName := flag.String("story", "gopher.json", "JSON file containing the story")
-    flag.Parse()
+func main() {
+	fileName := flag.String("story", "gopher.json", "JSON file containing the story")
+	flag.Parse()
 
-    file, err := os.Open(*fileName)
-    if err != nil {
-        exit(err)
-    }
-    defer file.Close()
+	file, err := os.Open(*fileName)
+	if err != nil {
+		exit(err)
+	}
+	defer file.Close()
 
-    story, err := cyoa.JsonStoryParser(file)
-    if err != nil {
-        exit(err)
-    }
+	story, err := cyoa.JSONStoryParser(file)
+	if err != nil {
+		exit(err)
+	}
 
-    fmt.Printf("%+v\n", story)
+	handler := cyoa.NewHandler(story)
+	port := 5000
+
+	log.Printf("Running on http://127.0.0.1:%d\n", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), handler))
 }
