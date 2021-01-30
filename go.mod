c5088433f0012010051576a8d248d9b6e1eaa69a
El Mehdi Rami <elmehdirami5@gmail.com>
2021-01-31 00:01:15 +0100

    sitemap parsing links

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.15
 require (
 	4d63.com/gochecknoglobals v0.0.0-20201008074935-acfc0b28355a // indirect
 	4d63.com/gochecknoinits v0.0.0-20200108094044-eb73b47b9fc4 // indirect
+	github.com/alecthomas/gocyclo v0.0.0-20150208221726-aa8f8b160214 // indirect
 	github.com/kisielk/errcheck v1.5.0 // indirect
 	github.com/mibk/dupl v1.0.0 // indirect
 	github.com/stripe/safesql v0.2.0 // indirect
--- sitemap/main.go
+++ sitemap/main.go
@@ -3,22 +3,47 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
+	"log"
 	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/ElMehdi19/gophercises/link"
 )
 
 func main() {
 	urlFlag := flag.String("url", "https://gophercises.com", "`Page URL` to build a sitemap for")
 	flag.Parse()
-	_ = *urlFlag
+
 	resp, err := http.Get(*urlFlag)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
+
+	links, _ := link.Parse(resp.Body)
+	reqURL := resp.Request.URL
+	baseURL := &url.URL{
+		Scheme: reqURL.Scheme,
+		Host:   reqURL.Host,
 	}
-	fmt.Println(string(body))
+	base := baseURL.String()
+	hrefs := getHrefs(links, base)
+
+	for _, href := range hrefs {
+		fmt.Println(href)
+	}
+}
+
+func getHrefs(links []link.Link, base string) []string {
+	var hrefs []string
+	for _, l := range links {
+		switch {
+		case strings.HasPrefix(l.Href, "/"):
+			hrefs = append(hrefs, base+l.Href)
+		case strings.HasPrefix(l.Href, "http"):
+			hrefs = append(hrefs, l.Href)
+		}
+	}
+	return hrefs
 }
