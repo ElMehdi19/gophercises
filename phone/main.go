f8ecff8327cad105b3200b020ae490d44237a19e
El Mehdi Rami <elmehdirami5@gmail.com>
2021-02-14 22:54:19 +0100

    phone sql insert func done

--- phone/main.go
+++ phone/main.go
@@ -30,6 +30,16 @@ func main() {
 	must(createPhoneNumsTable(db))
 }
 
+func insertPhone(db *sql.DB, phone string) (int, error) {
+	statement := "INSERT INTO phone_numbers (value) VALUES ($1) RETURNING id"
+	var id int
+	err := db.QueryRow(statement, phone).Scan(&id)
+	if err != nil {
+		return -1, err
+	}
+	return id, nil
+}
+
 func createPhoneNumsTable(db *sql.DB) error {
 	statement := `CREATE TABLE IF NOT EXISTS phone_numbers(
 		id SERIAL,
