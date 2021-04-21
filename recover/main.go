59c7831ee2b474f81c648e490cb635d2a7021c8c
El Mehdi Rami <lbiggboss19@gmail.com>
2021-04-21 00:38:35 +0000

    recover middleware

--- recover/main.go
+++ recover/main.go
@@ -15,20 +15,27 @@ func main() {
 	mux.HandleFunc("/panic-after", panicAfterDemo)
 
 	log.Printf("Running on http://127.0.0.1:%d\n", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), mux))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), recoverMw(mux)))
 }
 
 func funcThatPanics() {
 	panic("Oh geez Rick!!")
 }
 
+func recoverMw(h http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				log.Println(err)
+				http.Error(w, "Something went wrong", http.StatusInternalServerError)
+				return
+			}
+		}()
+		h.ServeHTTP(w, r)
+	}
+}
+
 func panicDemo(w http.ResponseWriter, r *http.Request) {
-	defer func() {
-		if err := recover(); err != nil {
-			log.Printf("%+v", err)
-			http.Error(w, "Something went wrong", http.StatusInternalServerError)
-		}
-	}()
 	funcThatPanics()
 }
 
